0ce1824e1e9bae3b237a2959ca647c609b962102
steevenU1 <154490794+steevenU1@users.noreply.github.com>
2024-11-11 22:06:38 -0600

    Detalles de películas

--- src/main.js
+++ src/main.js
@@ -19,6 +19,9 @@ function createMovies(movies, container){
 
         const movieContainer = document.createElement('div')
         movieContainer.classList.add('movie-container');
+        movieContainer.addEventListener('click', () => {
+            location.hash = '#movie=' + movie.id;
+        });
 
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
@@ -164,6 +167,23 @@ async function getMoviesBySearch(query) {
     createMovies(movies, genericSection);
 
     
+}
+
+async function getMovieById(id){
+    
+    const {data: movie} = await api('movie/' + id);
+
+    const movieImgUrl = 'https://image.tmdb.org/t/p/w500/' + movie.poster_path;
+    
+    headerSection.style.background = `
+    linear-gradient(180deg, rgba(0, 0, 0, 0.35) 19.27%, rgba(0, 0, 0, 0) 29.17%),
+    url(${movieImgUrl})`;
+
+    movieDetailTitle.textContent = movie.title;
+    movieDetailDescription.textContent = movie.overview;
+    movieDetailScore.textContent = movie.vote_average;
+
+    createCategories(movie.genres, movieDetailCategoriesList);
 }
 
 // getTrendingMoviesPreview();
--- src/navigation.js
+++ src/navigation.js
@@ -91,6 +91,9 @@ function movieDetailsPage() {
     categoriesPreviewSection.classList.add('inactive');
     genericSection.classList.add('inactive');
     movieDetailSection.classList.remove('inactive');
+
+    const [_, movieId] = location.hash.split('=');
+    getMovieById(movieId);
 }
 
 function searchPage() {
