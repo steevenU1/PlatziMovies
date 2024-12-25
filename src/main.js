088bbfbc9ce7344ea284a1ed0f85960a5bf71b2a
steevenU1 <154490794+steevenU1@users.noreply.github.com>
2024-12-25 17:51:20 -0600

    Lasy loading en imagenes

--- src/main.js
+++ src/main.js
@@ -10,7 +10,17 @@ const api = axios.create({
 
 //Helpers
 
-function createMovies(movies, container){
+const lazyLoader = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            const url = entry.target.getAttribute('data-img')
+            entry.target.setAttribute('src', url)
+        }
+        
+    });
+});
+
+function createMovies(movies, container, lazyLoad = false){
     container.innerHTML = '';
 
     movies.forEach(movie => {
@@ -26,7 +36,11 @@ function createMovies(movies, container){
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
         movieImg.setAttribute('alt', movie.title);
-        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path);
+        movieImg.setAttribute(lazyLoad ? 'data-img' : 'src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path);
+
+        if (lazyLoad){
+            lazyLoader.observe(movieImg);
+        }
 
         movieContainer.appendChild(movieImg);
         container.appendChild(movieContainer);
@@ -68,7 +82,7 @@ async function getTrendingMoviesPreview() {
 
     const movies = data.results;
 
-    createMovies(movies, trendingMoviesPreviewList);
+    createMovies(movies, trendingMoviesPreviewList, true);
 
     // trendingMoviesPreviewList.innerHTML = "";
 
